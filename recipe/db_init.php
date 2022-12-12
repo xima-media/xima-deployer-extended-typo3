545346b2255b15a43a8434b3fd7e47f5f0d1bb04
Maik Schneider <mas@xima.de>
2022-12-12 17:30:36 +0100

    fix: copy database from base branch

--- recipe/db_init.php
+++ recipe/db_init.php
@@ -3,7 +3,6 @@
 namespace Deployer;
 
 use SourceBroker\DeployerExtendedDatabase\Utility\ConsoleUtility;
-use SourceBroker\DeployerExtendedDatabase\Utility\DatabaseUtility;
 
 /**
  * Check for missing database: Run database updateschema + import database of base branch
@@ -12,28 +11,18 @@ task('db:init', function () {
 
     $baseBranch = (new ConsoleUtility())->getOption('base_branch') ?: '';
 
-    if (!empty(get('argument_stage'))) {
-        $verbosity = (new ConsoleUtility())->getVerbosityAsParameter();
-        $baseOption = $baseBranch ? '--options=base_branch:' . $baseBranch . ' ' : '';
-        $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
-        run('cd ' . $activePath . ' && {{bin/php}} {{bin/deployer}} db:init ' . $baseOption . $verbosity);
+    // abort if feature branch has already been configured
+    if (test('[ -f {{deploy_path}}/.dep/releases.extended ]') || !$baseBranch || !get('argument_stage')) {
+        return;
     }
 
-    // empty database: import from base branch
-    $tables = [];
-    $databaseUtility = new DatabaseUtility();
-    foreach (get('db_databases_merged') as $databaseCode => $databaseConfig) {
-        $tables = [...$databaseUtility->getTables($databaseConfig)];
-    }
-    if (empty($tables) && $baseBranch) {
+    $targetStage = get('argument_stage');
+    $baseStage = str_replace(strtolower(get('branch')), $baseBranch, $targetStage);
+    $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
 
-        $basePath = str_replace(get('branch'), $baseBranch, get('deploy_path'));
-        $activeBasePath = $basePath . '/' . (test('[ -L ' . $basePath . '/release ]') ? 'release' : 'current');
-        $exportCommand = 'cd ' . $activeBasePath . ' && {{bin/php}} {{bin/typo3cms}} database:export';
+    // update schema (db:import would fail with empty database)
+    run('cd ' . $activePath . ' && {{bin/php}} {{bin/typo3cms}} database:updateschema');
 
-        $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
-        $importCommand = '{{bin/php}} ' . $activePath . '/vendor/bin/typo3cms database:import';
-
-        runLocally($exportCommand . ' | ' . $importCommand);
-    }
+    // copy database from base branch
+    runLocally('{{local/bin/deployer}} db:copy ' . $baseStage . ' --options=target:' . $targetStage);
 });
