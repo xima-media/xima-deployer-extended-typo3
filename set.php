a015837d4da83be63176262cebdf13968ab11a38
Maik Schneider <mas@xima.de>
2023-01-06 17:52:50 +0100

    fix: path for local host

--- set.php
+++ set.php
@@ -11,7 +11,7 @@ set('writable_chmod_recursive', false);
 set('writable_chmod_mode', '2770');
 
 // local host is always needed
-host('local')->set('deploy_path', '/var/www/html');
+host('local')->set('deploy_path', getcwd());
 
 // read typo3 database connection from bin/typo3cms > AdditionalConfiguration.php > .env
 set('driver_typo3cms', true);
