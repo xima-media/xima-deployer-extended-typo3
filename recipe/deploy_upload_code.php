5e35da3540010a98068c429cdc86998b0b033a08
Maik Schneider <mas@xima.de>
2023-03-19 16:33:10 +0100

    fix: add -R to rsync of upload

--- recipe/deploy_upload_code.php
+++ recipe/deploy_upload_code.php
@@ -4,10 +4,10 @@ namespace Deployer;
 
 task('deploy:upload_code', function () {
     // upload files/folders
-    foreach(get('upload_paths') as $path) {
-        upload($path, '{{release_path}}/' . $path);
+    foreach (get('upload_paths') as $path) {
+        upload($path, '{{release_path}}/', ['options' => ['-R']]);
     }
-    
+
     // fix permissions
     run('find {{release_path}} -type d -exec chmod {{writable_chmod_mode}} {} \;');
     run('find {{release_path}} -type f -exec chmod 0640 {} \;');
