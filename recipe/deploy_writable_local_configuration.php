e354dd03a52a9f5bf754178bb905df63b473f0ed
Maik Schneider <mas@xima.de>
2023-08-14 11:09:48 +0200

    fix: check existence of LocalConfiguration.php before applying chmod

--- recipe/deploy_writable_local_configuration.php
+++ recipe/deploy_writable_local_configuration.php
@@ -4,5 +4,8 @@ namespace Deployer;
 
 task('deploy:writableLocalConfiguration', function () {
     $remotePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
-    run('chmod 660 ' . $remotePath . '/public/typo3conf/LocalConfiguration.php');
+    $confPath = $remotePath . '/public/typo3conf/LocalConfiguration.php';
+    if (test('[ -f ' . $confPath . ' ]')) {
+        run('chmod 660 ' . $confPath);
+    }
 });
