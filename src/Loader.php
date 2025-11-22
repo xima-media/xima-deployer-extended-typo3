392b7e4dc2059d82903884ae19b7b6668cd0161f
Maik Schneider <maik.schneider@xima.de>
2025-11-22 11:01:45 +0100

    feat: add new Loader

---
+++ src/Loader.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Xima\XimaDeployerExtendedTypo3;
+
+class Loader
+{
+    public function __construct()
+    {
+        new \SourceBroker\DeployerLoader\Load([
+            ['package' => 'sourcebroker/deployer-extended'],
+            ['get' => 'sourcebroker/deployer-typo3-media'],
+            ['get' => 'sourcebroker/deployer-typo3-database'],
+            ['get' => 'sourcebroker/deployer-typo3-deploy'],
+            ['package' => 'xima/xima-deployer-extended-typo3'],
+        ]);
+    }
+}
